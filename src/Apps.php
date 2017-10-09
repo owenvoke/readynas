02bd8e08f201331352b2eea9eb6094720011e523
pxgamer <owzie123@gmail.com>
2017-10-09 11:52:33 +0100

    Added Apps class with getAppsInfo() method

---
+++ src/Apps.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace pxgamer\ReadyNAS;
+
+use pxgamer\ReadyNAS\Requests;
+
+/**
+ * Class Apps
+ */
+class Apps extends Requests\Requester
+{
+    use Requests\StandardRequest;
+
+    /**
+     * Get information about all installed apps
+     *
+     * @return array|null
+     */
+    public function getAppsInfo()
+    {
+        $response = $this->sendRequest('LaunchableApp', 'LocalApp_Collection');
+
+        $result = $this->xmlToArray($response);
+
+        return $result;
+    }
+}
