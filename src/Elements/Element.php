12d3a589190e84c1d91906bfac98f03dc8d35309
pxgamer <owzie123@gmail.com>
2017-11-01 21:44:43 +0000

    Updated to use a switch for setting variable types

--- src/Elements/Element.php
+++ src/Elements/Element.php
@@ -19,6 +19,22 @@ trait Element
             foreach ($data as $item => $value) {
                 $itemLower = strtolower($item);
                 if (property_exists($this, $itemLower)) {
+                    switch (true) {
+                        case is_numeric((string)$value):
+                            $value = (int)$value;
+                            break;
+                        case (string)$value === 'true':
+                            $value = true;
+                            break;
+                        case (string)$value === 'false':
+                            $value = false;
+                            break;
+                        case (string)$value !== '':
+                            $value = (string)$value;
+                            break;
+                        default:
+                            $value = null;
+                    }
                     $this->$itemLower = $value;
                 }
             }
